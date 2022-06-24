d07be2cbf77a3d2ee033b262da8e468000d76ae3
SuperMindu <masuzuu@naver.com>
2022-06-24 14:13:54 +0900

    6/24 수업

--- keras/keras10_ddarung.py
+++ keras/keras10_ddarung.py
@@ -1,6 +1,7 @@
 # 데이콘 따릉이 문제풀이
 import numpy as np
 import pandas as pd   # 엑셀 데이터 불러올 때 사용
+import time
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
@@ -47,13 +48,11 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
 
 #2. 모델 구성
 model=Sequential()
-model.add(Dense(30, input_dim=9))
+model.add(Dense(100, input_dim=9))
 model.add(Dense(30))
 model.add(Dense(40))
 model.add(Dense(20))
 model.add(Dense(30))
-model.add(Dense(50))
-model.add(Dense(60))
 model.add(Dense(70))
 model.add(Dense(30))
 model.add(Dense(70))
@@ -64,8 +63,8 @@ model.add(Dense(1))
 
 #3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=800, batch_size=50)
-
+start_time = time.time()
+model.fit(x_train, y_train, epochs=380, batch_size=30)
 
 
 #4. 평가, 예측
@@ -74,23 +73,21 @@ print('loss : ', loss)
 
 y_predict = model.predict(x_test)
 
-def RMSE(y_test, y_predict): #괄호 안의 변수를 받아들인다 :다음부터 적용
+def RMSE(y_test, y_predict): #괄호 안의 변수를 받아들인다 :다음부터 적용 
     return np.sqrt(mean_squared_error(y_test, y_predict)) #루트를 씌워서 돌려줌 
 
-rmse = RMSE(y_test, y_predict)  #y_test와 y_predict를 비교해서 rmse로 출력 (원래 데이터와 예측 데이터를 비교)
+rmse = RMSE(y_test, y_predict)  #y_test와 y_predict를 비교해서 rmse로 출력 (원래 데이터와 예측 데이터를 비교) 
 print("RMSE : ", rmse)
 
 
-# loss :  2405.498046875
-# RMSE :  49.045877295397666
+end_time = time.time() - start_time
+print("걸린시간 : ", end_time)
+
+
+# loss :  2401.894287109375
+# RMSE :  49.00912287733187
 
 
 #과제1. 함수에 대해서 공부하기 
 #과제2. 낮은 로스값 스샷
-
-
-'''
-x_train, x_test, y_train, y_test = train_test_split(x, y, 
-        train_size=0.80, shuffle=True, random_state=72)
-        '''
 
