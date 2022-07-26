fa2b7755cd96ba287ef2ae40d6783736ed4e3e9e
SuperMindu <masuzuu@naver.com>
2022-07-26 18:05:40 +0900

    7/26

--- Personal_Project/98.test.py
+++ Personal_Project/98.test.py
@@ -19,18 +19,30 @@ elem.send_keys(Keys.RETURN) # enter 입력
 
 img_folder = f"d:/study_data/_data/image/PP/{keyword}" # 원하는 경로에 키워드 이름으로 폴더 자동생성
  
-if not os.path.isdir(img_folder) : # 폴더가 없으면 새로 생성하는 조건문 
-    os.mkdir(img_folder)
+# img_folder = './img'
+
+    
 
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd") # 이미지의 클래스 이름을 images 라는 변수에 담아주고 
-count = 1
-for image in images: # 이미지들 중에 각각 개별 이미지를 하나씩 뽑아서
-    image.click() # 그 이미지를 클릭하도록 해줌
-    time.sleep(2.5) # 로딩시간 감안해서 3초 대기하고
-    imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src") # url을 찾아서 
-    urllib.request.urlretrieve(imgUrl, "d:/study_data/_data/image/PP/", str(count) + ".jpg") # <- 이게 다운받을 폴더명 지정해서 받음
-    # urllib.request.urlretrieve(imgUrl, str(count) + ".jpg") # 다운 받도록 만들면 됨 
-    count = count + 1
+result = []
 
+if not os.path.isdir(img_folder) : # 없으면 새로 생성하는 조건문 
+    os.mkdir(img_folder)
+
+
+# for image in images: # 이미지들 중에 각각 개별 이미지를 하나씩 뽑아서
+#     image.click() # 그 이미지를 클릭하도록 해줌
+#     time.sleep(2.5) # 로딩시간 감안해서 3초 대기하고
+#     # imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src") # url을 찾아서 
+
+
+
+
+
+for index, link in enumerate(result) :
+    start = link.rfind('.')
+    end = link.rfind('?')
+    filetype = link[start:end]
+    urllib.request.urlretrieve(link, './imgs/{}{}'.format(index, filetype))
 
 time.sleep(3)
